f3efaa3c3f4647b39f1eab537bcd0e863e53cbf9
Daniel-05-lives <2425403457@live.gctu.edu.gh>
2026-05-31 03:02:37 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
-
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Solomon Daniel Ahenkan
+# STUDENT ID: 2425403457
+# Email: 2425403457@gctu.live.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Solomon Daniel Ahenkan, this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
-  pass #code goes here
-
-
+    # This function prints the initials for SDA.
+    print()
+    print("...SDA")
+    print()
+    print(".....SSSSSSSSS...........DDDDDDDDD...............AAAAAA........")
+    print("....SSSSSSSSSS...........DDDDDDDDDD.............AAAAAAAA.......")
+    print("...SSSS..................DD........DD..........AAA....AAA......")
+    print("...SSSS..................DD........DD.........AA........AA.....")
+    print(".....SSSSSS..............DD........DD.........AA........AA.....")
+    print(".......SSSSSS............DD........DD.........AAAAAAAAAAAA.....")
+    print("...........SSSS..........DD........DD.........AA........AA.....")
+    print("...........SSSS...**.....DD........DD....**...AA........AA...**")
+    print("...SSSSSSSSSSS....**.....DDDDDDDDDDD.....**...AA........AA...**")
+    print("...SSSSSSSSSS.....**.....DDDDDDDDD.......**...AA........AA...**")
+    print()
 
 main()
